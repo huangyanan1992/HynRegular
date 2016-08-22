e2eb53e89bb9739c352f45668fbcdc08d6040b1d
SQHyn1992 <huangyanan@01zhuanche.com>
2016-08-22 11:59:34 +0800

    Initial Commit

---
+++ HynRegular/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HynRegular
+//
+//  Created by Huang Yanan on 16/8/22.
+//  Copyright © 2016年 Huang Yanan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
